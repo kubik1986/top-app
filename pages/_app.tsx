1ce38e4e026a8a785faae18b280c09675ef6db5f
Viacheslav Akulov <sentence@ngs.ru>
2021-07-14 15:35:52 +0700

    Add component 'Document'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
+import React from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
