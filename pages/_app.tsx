c8a0f975e8a952b635409b4b3c50fb746b0761a8
Viacheslav Akulov <sentence@ngs.ru>
2021-07-15 21:19:40 +0700

    Update global styles and create 'Htag' component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,12 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>MyTop - наш лучший топ</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+        />
       </Head>
       <Component {...pageProps} />
     </>
