48cad87b135081b873a3109cb46016e9b81e2e6f
Viacheslav Akulov <sentence@ngs.ru>
2021-07-16 13:28:15 +0700

    Create 'Button' component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="transparent" arrow="down">
+        Кнопка
+      </Button>
     </>
   );
 }
