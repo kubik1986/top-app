3c80678ee905a94c02aa416565f9a16b7cae9013
Viacheslav Akulov <sentence@ngs.ru>
2021-07-16 22:26:16 +0700

    Create 'Tag' component

---
+++ components/Tag/Tag.props.ts
@@ -0,0 +1,9 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+export interface TagProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  size?: 's' | 'm';
+  color?: 'transparent' | 'red' | 'gray' | 'green' | 'primary';
+  href?: string;
+  children: ReactNode;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag, Ptag } from '../components';
+import { Button, Htag, Ptag, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -15,6 +15,39 @@ export default function Home(): JSX.Element {
       <Ptag textSize="s">Маленький</Ptag>
       <Ptag textSize="m">Средний</Ptag>
       <Ptag textSize="l">Большой</Ptag>
+      <Tag>Мал прозр</Tag>
+      <Tag size="m">Бол прозр</Tag>
+      <Tag size="s" color="red">
+        Мал крас
+      </Tag>
+      <Tag size="m" color="red">
+        Бол крас
+      </Tag>
+      <Tag color="gray">Мал сер</Tag>
+      <Tag size="m" color="gray">
+        Бол сер
+      </Tag>
+      <Tag color="green">Мал зел</Tag>
+      <Tag size="m" color="green">
+        Бол зел
+      </Tag>
+      <Tag color="primary">Мал осн</Tag>
+      <Tag size="m" color="primary">
+        Бол осн
+      </Tag>
+      <Tag href="#">Мал прозр ссылка</Tag>
+      <Tag color="red" href="#">
+        Мал крас ссылка
+      </Tag>
+      <Tag color="gray" href="#">
+        Мал сер ссылка
+      </Tag>
+      <Tag color="green" href="#">
+        Мал зел ссылка
+      </Tag>
+      <Tag color="primary" href="#">
+        Мал осн ссылка
+      </Tag>
     </>
   );
 }
