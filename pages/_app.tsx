1c3882a8c534904ec7d6b952b2f1759b082a9052
Viacheslav Akulov <sentence@ngs.ru>
2021-07-21 21:57:33 +0700

    Update 'Sidebar' component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
         <title>MyTop - наш лучший топ</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
