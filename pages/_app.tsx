5fa5acf22390a1d7099784bdfbfca97119497b63
Viacheslav Akulov <sentence@ngs.ru>
2021-08-05 23:45:21 +0700

    Add <meta> tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,10 @@ import Head from 'next/head';
 import React from 'react';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
         <title>MyTop - наш лучший топ</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -23,6 +19,13 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta property="og:locale" content="ru_RU" />
+        <meta property="og:type" content="article" />
       </Head>
       <Component {...pageProps} />
     </>
